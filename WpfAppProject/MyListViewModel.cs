d4ee0ed68fb11b203c4a9a51ada43598be402d69
makimoke <maki_k5@yahoo.co.jp>
2025-07-12 15:36:34 +0900

    リストビュー表示テスト

---
+++ WpfAppProject/MyListViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+
+namespace WpfAppProject
+{
+    public class PersonInfo
+    {
+        public int no { get; set; }
+        public string name { get; set; }
+        public string birthday { get; set; }
+        public string seibetu { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string mail { get; set; }
+    }
+    class MyListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private double clientWidth;
+        public double ClientWidth
+        {
+            get
+            {
+                return clientWidth;
+            }
+            set
+            {
+                clientWidth = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double clientHeight;
+        public double ClientHeight
+        {
+            get
+            {
+                return clientHeight;
+            }
+            set
+            {
+                clientHeight = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private List<PersonInfo> personDatas = new List<PersonInfo>();
+        public List<PersonInfo> PersonDatas
+        {
+            get
+            {
+                return personDatas;
+            }
+        }
+        public PersonInfo PersonDataAdd
+        {
+            set
+            {
+                PersonInfo data = value;
+                data.no = personDatas.Count + 1;
+                personDatas.Add(data);
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
---
+++ WpfAppProject/MyListWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppProject
+{
+    /// <summary>
+    /// MyListWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MyListWindow : Window
+    {
+        private MyListViewModel _viewModel;
+
+        public MyListWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new MyListViewModel();
+
+            DataContext = _viewModel;
+
+            var person1 = new PersonInfo();
+            person1.name = "東京太郎";
+            person1.birthday = "19XX/01/01";
+            person1.seibetu = "男";
+            person1.address = "東京都XX区YY 〇-〇-〇";
+            person1.phone = "090-XXXX-YYYY";
+            person1.mail = "hoge@hoge.com";
+
+            var person2 = new PersonInfo();
+            person2.name = "大阪花子";
+            person2.birthday = "20XX/02/02";
+            person2.seibetu = "女";
+            person2.address = "大阪府XX市YY 〇-〇-〇";
+            person2.phone = "080-XXXX-YYYY";
+            person2.mail = "sample@sample.com";
+
+            var person3 = new PersonInfo();
+            person3.name = "北海道次郎";
+            person3.birthday = "20XX/02/02";
+            person3.seibetu = "男";
+            person3.address = "沖縄県XX市YY 〇-〇-〇";
+            person3.phone = "080-XXXX-YYYY";
+            person3.mail = "hogo@hogo.com";
+
+            _viewModel.PersonDataAdd = person1;
+            _viewModel.PersonDataAdd = person2;
+            _viewModel.PersonDataAdd = person3;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            FrameworkElement frameworkElement = Content as FrameworkElement;
+            _viewModel.ClientWidth = frameworkElement.ActualWidth;
+            _viewModel.ClientHeight = frameworkElement.ActualHeight;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyListView.UnselectAll();
+        }
+    }
+}
